0b0f89c916ca0605267d2628170b3bb2f528c00d
DineshDan <dineshdanchai341@gmail.com>
2020-11-24 09:48:35 +0530

    Modified: Logic to check as simulator multiple times of flipping a coin and show number of times head and tail has won

--- Flipcoin.sh
+++ Flipcoin.sh
@@ -4,9 +4,25 @@ echo "Welcom of the flip coin simulation program being solved in the Master bran
 		if(($random==1))
 		then
 		     echo "Heads winner"
+		read -p "enter a range to flipcoin for multiple times: " n
+		while(( $n!=0 ))
+		do
+		    random=$((RANDOM%2))
+		    echo "$random"
+		    if(($random==1))
+		    then
+		        ((headcount++))
+		
+
+		    else
+		        ((tailcount++))
+		    fi
+		((n--))
+		done
+		echo "$headcount times Heads won"
+		echo "$tailcount times Tails won"
 		
 
 		else
 		     echo "Tails winner"
 		fi
-
