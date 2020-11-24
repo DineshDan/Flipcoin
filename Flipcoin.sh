2d21341c883ad4875abc0ca00a00a4dd7a644406
DineshDan <dineshdanchai341@gmail.com>
2020-11-24 09:43:42 +0530

    Modified: Logic to check as a simulator start with flipping a coin to display heads or tails as winner

--- Flipcoin.sh
+++ Flipcoin.sh
@@ -1,2 +1,12 @@
-echo "Welcome This problem displays the winner Heads or Tails"
+echo "Welcom of the flip coin simulation program being solved in the Master branch"
+		random=$((RANDOM%2))
+		echo "$random"
+		if(($random==1))
+		then
+		     echo "Heads winner"
+		
+
+		else
+		     echo "Tails winner"
+		fi
 
