bb477051d991c3a6a74eef78e227508eaa9f3e54
DineshDan <dineshdanchai341@gmail.com>
2020-11-24 11:41:58 +0530

    Added: Logic to check till the difference if minimum 2 points is achieved

--- Flipcoin.sh
+++ Flipcoin.sh
@@ -1,29 +1,16 @@
-#!/bin/bash -x
-echo "Welcome of the flip coin simulation program being solved in the Master branch"
-read -p "enter a range to flipcoin for multiple times: " n
-headcount=0
-tailcount=0
-for((i=0; i<=$n; i++))
-	while (($headcount !=21 && $tailcount !=21))
-	do
+echo "Welcom of the flip coin simulation program being solved in the Master branch"
+		read -p "enter a range to flipcoin for multiple times: " n
+		while(( $n!=0 ))
+		headcount=0
+		tailcount=0
+		while(( $headcount!=21 && $tailcount!=21 ))
+		do
 		    random=$((RANDOM%2))
 		    echo "$random"
-		    if(($random==1))
-		    then
-		         headcount=$((headcount + 1))
-
-		    else
-		          tailcount=$((tailcount + 1))
-
-
-		    fi
 		done
-		if(($headcount == $tailcount ))
-		    then
-		          echo "tie"
-		    fi
-
-
+		if(( $headcount == $tailcount ))
+		then
+		     echo "tie"
+		fi
 		echo "$headcount times Heads won"
 		echo "$tailcount times Tails won"
-
